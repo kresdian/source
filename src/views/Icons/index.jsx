0dd739092e7161a4f7098c9789be094907955936
Kres <53458633+kresdian@users.noreply.github.com>
2019-10-11 12:50:45 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core';
+
 
 const useStyles = makeStyles(theme => ({
   root: {

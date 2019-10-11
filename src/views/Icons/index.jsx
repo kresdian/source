93ce301c81041c3d3d471cd28b57a0912be32488
Kres <53458633+kresdian@users.noreply.github.com>
2019-10-11 12:45:30 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-// Externals
-import PropTypes from 'prop-types';
-
-// Material helpers
-import { withStyles } from '@material-ui/core';
-
-// Shared layouts
-import { Dashboard as DashboardLayout } from 'layouts';
-
-// Component styles
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing.unit * 4
+    padding: theme.spacing(3, 2),
   },
-  iframe: {
-    width: '100%',
-    minHeight: '640px',
-    border: 0
-  }
-});
-
+}));
 class Icons extends Component {
   render() {
     const { classes } = this.props;
@@ -28,10 +15,17 @@ class Icons extends Component {
     return (
       <DashboardLayout title="Icons">
         <div className={classes.root}>
-      <iframe src="https://detik.com" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe> </div>
-      </DashboardLayout>
-    );
-  }
+     <div>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </Paper>
+    </div>
+  );
 }
 
 Icons.propTypes = {
